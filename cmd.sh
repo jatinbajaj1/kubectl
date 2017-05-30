a1c7885bbf38afac096be8b23dc23f6dab681f22
jatinbajaj1 <jatinbajaj.mca@gmail.com>
2017-05-30 14:22:15 +0530

    added new file

---
+++ cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while :
+do 
+     ### add some input and output here ###
+     case $var in
+       yes) do something ;;
+       no) do something ;;
+       quit) break ;;        ##Abandon the loop.
+     ease
+done
